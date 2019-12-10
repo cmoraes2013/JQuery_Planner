a7692ba07c9dc0a766de44e04b663c8dcf0fc9bb
Clarisse Moraes <clarissem95@live.com>
2019-12-10 17:08:58 -0500

    Added table and Save buttons

---
+++ Psuedocode.md
@@ -0,0 +1,4 @@
+get item from local storage
+saving to local storage
+comparing present time to when you need to do it
+    color code gray past, green is future, red is now
---
+++ script.js
@@ -0,0 +1,4 @@
+//First we need to display the current day/time
+$("#currentDay").text(moment().format('MMMM Do YYYY'));
+// Next, we need to color code the to-do items from the past, present, and future
+
